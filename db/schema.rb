c2cdd1b640c3fe81b9003161fa99a908c891069e
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-12-11 18:23:35 -0500

    added skills model and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_222545) do
+ActiveRecord::Schema.define(version: 2018_12_11_225434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2018_12_11_222545) do
   end
 
   create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_img"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Jujubes jujubes fruitcake biscuit cheesecake candy canes. Sugar plum cake sweet roll halvah powder gummi bears pudding. Oat cake pie sesame snaps bear claw brownie. Biscuit donut cookie jelly chocolate powder icing. "
+  )
+end
+puts "10 blogs cerated"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+puts "5 skills created"
+
+9.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio Title #{portfolio}" ,
+    subtitle: "My Great Srvice",
+    body: "Jujubes jujubes fruitcake biscuit cheesecake candy canes. Sugar plum cake sweet roll halvah powder gummi bears pudding. Oat cake pie sesame snaps bear claw brownie. Biscuit donut cookie jelly chocolate powder icing." ,
+    main_img: "https://place-hold.it/600x400"  ,
+    thumb_image: "https://place-hold.it/350x200" ,
+  )
+end
+puts "9 portfolio items created"
