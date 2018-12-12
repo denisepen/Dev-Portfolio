efc92d397a1245e927e47ae0fa1dd10f83d0e165
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-12-11 21:27:53 -0500

    implemented delete for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,58 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      redirect_to @portfolio
+      else
+        render :new
+    end
+  end
+
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolio_path(@portfolio), notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def destroy
+
+    @portfolio = Portfolio.find(params[:id])
+    @portfolio.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :body, :subtitle, :main_img, :thumb_image)
+
+  end
 end
